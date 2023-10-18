cb882c0c9951f07ebd521d87e7f0f5c22c360817
johnpauljavier <johnpauljavier0821@gmail.com>
2023-10-19 05:23:57 +0800

    first commit

---
+++ script.js
@@ -0,0 +1,19 @@
+const quote = document.querySelector("#quote");
+const author = document.querySelector("#author");
+const btn = document.querySelector("#btn");
+
+btn.addEventListener("click", getQuote);
+
+function getQuote() {
+    fetch("http://quotable.io/random")
+    .then(result => result.json())
+    .then(data => {
+        quote.innerHTML = `"${data.content}
+        "`;
+        author.innerHTML = data.author;
+    })
+    .catch(error => console.log('error', error));
+}
+
+
+
